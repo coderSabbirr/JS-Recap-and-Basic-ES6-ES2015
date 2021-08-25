a1b27ea0ecee388fa30edbd95df516f82c25d728
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-08-25 13:25:55 +0600

    arrow funtion

---
+++ tempalte-string.js
@@ -0,0 +1,11 @@
+const multiLine = `Amar name sabbir. kamon aso sobai .`
+const count =5; 
+const new1 = `<h3 class="friend-name">friend-${count}</h3>`
+console.log(new1);
+
+
+const firstName ='sabbir';
+const lastName ='nahid';
+// tampalte string ,daynamic string
+const fullName =`${firstName} ${lastName}`
+console.log(fullName);
