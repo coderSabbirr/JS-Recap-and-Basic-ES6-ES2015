e3ded76c174c79ceac7017f8e2d70836d7d2acbe
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-08-25 13:41:06 +0600

    more arrow function

--- arrow-function.js
+++ arrow-function.js
@@ -18,15 +18,20 @@ const add4 = (num1, num2) => num1 + num2;
 
 
 // document.getElementById('mybutton').onclick = function(){
+    const add5 = (num1, num2) => num1 - num2;
 
 // }
 const sum1 = add(15,15);
 const sum2 = add2(15,15);
 const sum3 = add3(15,15);
 const sum4 = add4(15,15);
+const sum5 =add5(80,70);
+
+// console.log(sum1);
+// console.log(sum2);
+// console.log(sum3);
+// console.log(sum4);
+console.log(sum5);
 
 
-console.log(sum1);
-console.log(sum2);
-console.log(sum3);
-console.log(sum4);
+
---
+++ more-arrow-function.js
@@ -0,0 +1,15 @@
+const add = (num1, num2) => num1 + num2 ;
+
+
+const multiply =(num1, num2, num3) => num1 *num2 * num3;
+
+const sum =add(20,20);
+const result = multiply(10, 10, 10)
+
+console.log(sum);
+console.log(result);
+
+
+const getName =() => 'jhon deo'
+const name = getName();
+console.log(name);
